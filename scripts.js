efb71e1777c00934ad80d2aad39d3c247aa26efb
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-05-14 21:02:29 +0800

    Added gameround functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,48 @@
+playerPlay = prompt("Rock Paper Scissors?!");
+
 function computerPlay(){
-    var cpu_selections = ["Rock", "Paper", "Scissors"];
-    return cpu_selections[Math.floor(Math.random() * 2.9)]
+    var computerSelection = ["Rock", "Paper", "Scissors"];
+    const num = Math.floor(Math.random() * 2.9)
+    return computerSelection[num];
 }
 
-console.log(computerPlay())
+function gameRound(playerSelection, computerSelection){
+    if (playerSelection == "Rock" || playerSelection == "ROCK" || playerSelection == "rock"){
+        if (computerSelection == "Rock"){
+            return "DRAW! Rock against rock."
+        }
+        else if (computerSelection == "Paper"){
+            return "YOU WIN! Rock against paper."
+        }
+        else if (computerSelection == "Scissors"){
+            return "YOU LOSE! Rock against scissors."
+        }
+    }
+    else if (playerSelection == "Paper" || playerSelection == "PAPER" || playerSelection == "paper"){
+        if (computerSelection == "Rock"){
+            return "YOU WIN! Paper against rock."
+        }
+        else if (computerSelection == "Paper"){
+            return "DRAW! Paper against paper."
+        }
+        else if (computerSelection == "Scissors"){
+            return "YOU LOSE! Paper against scissors."
+        }
+    }
+    else if (playerSelection == "Scissors" || playerSelection == "SCISSORS" || playerSelection == "scissors"){
+        if (computerSelection == "Rock"){
+            return "YOU LOSE! Scissors against rock."
+        }
+        else if (computerSelection == "Paper"){
+            return "YOU WIN! Scissors against paper."
+        }
+        else if (computerSelection == "Scissors"){
+            return "DRAW! Scissors against scissors."
+        }
+    }
+    else{
+        return "Please type in 'rock', 'paper', or 'scissors'"
+    }
+}
+
+console.log(gameRound(playerPlay, computerPlay()));
