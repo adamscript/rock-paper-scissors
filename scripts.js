0d43e86522aa08a1ffef277f3c60f2341aa9f06d
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-05-14 21:27:00 +0800

    Added game and score functionality

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-playerPlay = prompt("Rock Paper Scissors?!");
+let playerScore;
+let computerScore;
 
 function computerPlay(){
     var computerSelection = ["Rock", "Paper", "Scissors"];
@@ -12,28 +13,34 @@ function gameRound(playerSelection, computerSelection){
             return "DRAW! Rock against rock."
         }
         else if (computerSelection == "Paper"){
+            playerScore += 1;
             return "YOU WIN! Rock against paper."
         }
         else if (computerSelection == "Scissors"){
+            computerScore += 1;
             return "YOU LOSE! Rock against scissors."
         }
     }
     else if (playerSelection == "Paper" || playerSelection == "PAPER" || playerSelection == "paper"){
         if (computerSelection == "Rock"){
+            playerScore += 1;
             return "YOU WIN! Paper against rock."
         }
         else if (computerSelection == "Paper"){
             return "DRAW! Paper against paper."
         }
         else if (computerSelection == "Scissors"){
+            computerScore += 1;
             return "YOU LOSE! Paper against scissors."
         }
     }
     else if (playerSelection == "Scissors" || playerSelection == "SCISSORS" || playerSelection == "scissors"){
         if (computerSelection == "Rock"){
+            computerScore += 1;
             return "YOU LOSE! Scissors against rock."
         }
         else if (computerSelection == "Paper"){
+            playerScore += 1;
             return "YOU WIN! Scissors against paper."
         }
         else if (computerSelection == "Scissors"){
@@ -45,4 +52,22 @@ function gameRound(playerSelection, computerSelection){
     }
 }
 
-console.log(gameRound(playerPlay, computerPlay()));
+function game(){
+    playerScore = 0;
+    computerScore = 0;
+    for(let i = 1; i <= 5; i++){
+        playerPlay = prompt("Rock Paper Scissors?!");
+        console.log(gameRound(playerPlay, computerPlay()))
+    }
+    if (playerScore > computerScore){
+        console.log("YOU ARE THE WINNER!");
+    }
+    else if (playerScore < computerScore){
+        console.log("COMPUTER IS THE WINNER!");
+    }
+    else {
+        console.log("IT'S A DRAW!");
+    }
+}
+
+game()
